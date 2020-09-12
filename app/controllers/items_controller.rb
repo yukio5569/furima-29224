27091e5d230d9cab1ad2a3e1002ac505189dd769
yukio5569 <pekapeka777@icloud.com>
2020-09-12 15:42:19 +0900

    商品出品機能の実装　修正3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,7 +13,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path(@item)
+      redirect_to root_path
     else
       render :new
     end
