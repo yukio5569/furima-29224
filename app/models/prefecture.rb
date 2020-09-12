f7717dd00e2ece14d57992b6a24c896b11be746f
yukio5569 <pekapeka777@icloud.com>
2020-09-12 15:21:01 +0900

    商品出品機能の実装　修正2

--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -19,3 +19,5 @@ class Prefectures < ActiveHash::Base
                {id: 45, name: '宮崎県'}, {id: 46, name: '鹿児島県'}, {id: 47, name: '沖縄県'}
               ]
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to:"items#index"
-  get 'items/new', to: 'items#new'
   resources :items, only: [:index, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
