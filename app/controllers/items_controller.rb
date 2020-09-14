dad11f65d19fa770ecd046f487ea22be067f0468
yukio5569 <pekapeka777@icloud.com>
2020-09-14 16:56:08 +0900

    商品一覧表示機能の修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :user_signin, only: [:new]
   
    def index
-     @items = Item.all.order("created_at DESC")
+     @items = Item.order("created_at DESC")
    end
 
   def new
