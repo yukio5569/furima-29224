7cbc78ff1307525ae1f1e04df180daa40dbaffc4
yukio5569 <pekapeka777@icloud.com>
2020-09-15 17:00:01 +0900

    商品編集機能の修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,9 +30,12 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to items_path
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to items_path
+    else
+      render :edit
+    end
   end
 
   private
