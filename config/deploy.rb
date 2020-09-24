8de3171abd1015bc79c6dd982cb1457275e5bef6
yukio5569 <pekapeka777@icloud.com>
2020-09-24 19:24:34 +0900

    自動デプロイの準備

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+lock '3.14.1'
+
+set :application, 'furima-29224'
+
+set :repo_url,  'git@github.com:yukio5569/furima-29224.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/yukio5569.pem'] 
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
